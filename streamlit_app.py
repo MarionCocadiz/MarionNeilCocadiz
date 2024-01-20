8fce3831d1313696573ed918f783b61cab26dea2
MarionCocadiz <146499657+MarionCocadiz@users.noreply.github.com>
2024-01-20 21:59:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,11 @@ st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 st.subheader("Hi, I am Marion :wave:")
 st.title("I am a CCS student from Grade 10 Fortitude")
-st.write("I love studying random stuff and learning new things with this link i learn alot. ")
-st.write("[Learn More >](https://pythonandvba.com)")
+st.write("I'm going to teach you how to download Minecraft for free. ")
+
+st.write("This is the link . ")
+
+
+st.write("[Learn More >](https://tlauncher.org/en/)")
+
+
