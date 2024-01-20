2e5dd3fd2abaaa965f56a7ddda885deac7a8f08a
MarionCocadiz <146499657+MarionCocadiz@users.noreply.github.com>
2024-01-20 19:39:59 +0800

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="My Webpage", page_icon=:"tada:", layout="wide")
+
+st.subheader("Hi, I am Marion :wave:")
+st.title("A data analyst From Philippines")
+st.write("I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings. ")
+st.write("[Learn More >](https://pythonandvba.com)")
