a5183958a3b8e72f2c31d091f3ef39f27781b64c
MarionCocadiz <146499657+MarionCocadiz@users.noreply.github.com>
2024-01-21 16:55:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,10 +35,9 @@ with st.container():
 
         )
 
-st.write("More information about Philippines. ")
 
 
-st.write("[Learn more >](st.write("For more info click the link. ")
+st.write("[Learn more >]("For more info click the link. ")
 
 
 st.write("[Learn more >](https://www.topuniversities.com/blog/10-surprising-facts-about-asia)"))")
